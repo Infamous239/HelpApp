0f6cf0e274569f536d204c6505be453df7cc04cb
19546 <anthonycirilo@icloud.com>
2025-08-31 16:04:59 -0400

    Initial Commit

---
+++ Variables.py
@@ -0,0 +1,14 @@
+result = 5 * 6
+print("The answer to your query is",result)
+result = 8 / 3
+print("The answer to your query is",result)
+result = 8 // 3
+print("The answer to your query is",result)
+remainder = 8 % 3
+print("The answer to your query is",remainder)
+result = 4200000 + 23090
+print("The answer to your query is",result)
+result = 4200000 - 7633
+print("The answer to your query is",result)
+name = "Ernest Cirilo"
+print(name)
